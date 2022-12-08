882feb42a23e9b8236c6b8b2c3df62e4d4057835
alok <alok60541@gmail.com>
2022-12-08 23:53:26 +0530

    api call + logic done

--- 4_Qr_code_generator/script.js
+++ 4_Qr_code_generator/script.js
@@ -1,8 +1,31 @@
 // alert("it's working");
 
 const wrapper = document.querySelector(".wrapper");
+const qrInput = wrapper.querySelector(".form input");
+const qrImg = wrapper.querySelector(".qr-code img");
 const generateBtn = wrapper.querySelector(".form button");
 
 generateBtn.addEventListener("click", ()=> {
-    wrapper.classList.add('active');
+    let qrValue = qrInput.value;
+    
+    if(!qrValue) return; // if the input is empty then return from here
+
+    generateBtn.innerText = "Generating QR Code....";
+
+    // **** QR generator api link: https://goqr.me/api/ ****
+
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add('active');
+        generateBtn.innerText = "Generate QR Code";
+    })
+
+});
+
+// Remove QR code if no value in input
+qrInput.addEventListener("keyup", () => {
+    if(qrInput.value) {
+        wrapper.classList.remove("active");
+    }
 });
