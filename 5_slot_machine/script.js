26f8bbe4b688180188d51bfe3346bb984788157e
Alok Kumar <alok60541@gmail.com>
2023-05-06 22:59:22 +0530

    fn to collect bet amount

--- 5_slot_machine/script.js
+++ 5_slot_machine/script.js
@@ -29,7 +29,37 @@ const deposit = () => {
     }
 };
 
-const depositeAmount = deposit();
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of line to bet on (1-3): ");
+
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the total bet per line: ");
+
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > (balance / lines)) {
+            console.log("Invalid bet, try again.");
+        } else {
+            return numberBet;
+        }
+    }
+};
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
 
 
 
@@ -37,3 +67,4 @@ const depositeAmount = deposit();
 
 // ********** Question to be solve/know more about *******
 // 1. Why parseFloat() convert (34dfd) into 34 and throw no error, while show error on (dfd34).
+// 2. Is this necessary to terminate arrow function with ';' at end.
